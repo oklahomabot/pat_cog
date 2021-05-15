1b36aa733c9bd30e899d9749ee26199b1685b8c1
oklahoma_bot <77229043+oklahomabot@users.noreply.github.com>
2021-05-15 09:22:21 -0500

    Add files via upload

--- cogs/pat.py
+++ cogs/pat.py
@@ -1,8 +1,10 @@
 import os
 import discord
+import aiohttp
 from discord.ext import commands
 from PIL import Image
 from io import BytesIO
+from functools import partial
 
 
 class pat(commands.Cog):
@@ -10,20 +12,15 @@ class pat(commands.Cog):
         self.client = client
         self.owner = discord.ClientUser
 
-    @commands.command(aliases=['pet_pat'], hidden=False)
-    async def pat(self, ctx, user=None):
-        '''
-        Good Boy ...
-        Returns gif image using mentioned user
-        '''
+        # create a ClientSession
+        self.session = aiohttp.ClientSession(loop=client.loop)
 
-        user = await get_guild_member(ctx, user)
+    @staticmethod
+    def make_pat_gif(avatar):
         folder = os.path.join('./', 'cogs', 'pat', 'images')
         sizex, sizey = 112, 112
-        avatar = await make_avatar(user)
         avatar_sizes = [(90, 70), (90, 70), (95, 60), (97, 55),
                         (100, 50), (97, 55), (95, 60), (90, 70)]
-
         background = Image.open(os.path.join(folder, 'background.png'))
         frames = []
         for img_num in range(8):
@@ -37,11 +34,24 @@ class pat(commands.Cog):
                 av, dest=((sizex-av_width)//2, sizey-av_height))
             frame.alpha_composite(hand, dest=((0, int(1.5*(70-av_height)))))
             frames.append(frame)
-
         outfile = os.path.join(folder, 'output.gif')
         frames[0].save(outfile, 'GIF', disposal=2, transparency=0, save_all=True,
                        append_images=frames[1:], optimize=False, duration=12, loop=0)
+        return outfile
 
+    @commands.command(aliases=['pet_pat'], hidden=False)
+    async def pat(self, ctx, user=None):
+        '''
+        Good Boy ...
+        Returns gif image using mentioned user
+        '''
+
+        user = await get_guild_member(ctx, user)
+        avatar = await make_avatar(user)
+
+        # create partial function so we don't have to stack the args in run_in_executor
+        fn = partial(self.make_pat_gif, avatar)
+        outfile = await self.client.loop.run_in_executor(None, fn)
         await ctx.send(file=discord.File(outfile))
         os.remove(outfile)
 
